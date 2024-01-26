1eaf31b8541012ab43e8838c19c74595afe54ab7
sarahd-wu <sarahd.wu6@gmail.com>
2024-01-26 17:17:18 -0500

    its weird now. but at least you can create subcomponents?

--- test-form/src/App.js
+++ test-form/src/App.js
@@ -7,63 +7,10 @@ function App(props) {
 
   // Checks if the user is hovering over the component
   const [components, setComponents] = useState(null);
-  const [hoveredIndex, setHoveredIndex] = useState(null);
 
   useEffect(() => {
     setComponents(componentsData["Components"]);
   }, []);
-
-  const addNewComponent = (index) => {
-    // Assuming your new component data is entered through some UI form
-    const newComponent = {
-      Title: "New Component",
-      Content: "New Content",
-      Subcomponents: {}
-    };
-
-    // Update the state with the new component inserted at the specified index
-    setComponents((prevComponents) => {
-      const newComponents = { ...prevComponents };
-      const newKey = Date.now().toString();
-
-      // Insert the new component at the specified index
-      const keys = Object.keys(newComponents);
-      keys.splice(index, 0, newKey);
-
-      // Create a copy of the components with the new component added
-      const updatedComponents = keys.reduce((acc, key) => {
-        acc[key] = key === newKey ? newComponent : newComponents[key];
-        return acc;
-      }, {});
-
-      return updatedComponents;
-    });
-  };
-
-
-  const makeSubcomponent = (index) => {
-    if (index !== null && index > 0) {
-      setComponents((prevComponents) => {
-        const newComponents = { ...prevComponents };
-  
-        // Get the selected component and the one above it
-        const selectedKey = Object.keys(newComponents)[index];
-        const aboveKey = Object.keys(newComponents)[index - 1];
-  
-        // Make a copy of the selected component
-        const selectedComponent = { ...newComponents[selectedKey] };
-  
-        // Remove the selected component from the top level
-        delete newComponents[selectedKey];
-  
-        // Make it a subcomponent of the component above
-        newComponents[aboveKey].Subcomponents = newComponents[aboveKey].Subcomponents || {};
-        newComponents[aboveKey].Subcomponents[selectedKey] = selectedComponent;
-  
-        return newComponents;
-      });
-    }
-  };
 
   const updateJson = (index, newTitle, key) => {
     setComponents((prevComponents) => {
@@ -108,11 +55,6 @@ function App(props) {
           const subcomponents = component["Subcomponents"];
 
           return (
-            <div
-              key={componentKey}
-              onMouseEnter={() => setHoveredIndex(index)}
-              onMouseLeave={() => setHoveredIndex(null)}
-            >
 
               <Component
                 label={componentKey}
@@ -123,21 +65,11 @@ function App(props) {
                 index={index}
                 updateJson={updateJson}
               />
-
-              {hoveredIndex === index && (
-                <div>
-                  <button onClick={() => addNewComponent(index + 1)}> Add New Component </button>
-                  <button onClick={() => makeSubcomponent(index)}> Make Subcomponent </button>
-                </div>
-              ) }
-
-              
-            </div>
             
           );
         })}
         
-    {/* Content List Section */}
+    {/* Content List Section 
     <div>
         <h1>Content List</h1>
         { components &&
@@ -177,8 +109,8 @@ function App(props) {
             </div>
             );
           })}
-        </div>
-    </div>
+    </div> */}
+    </div> 
   );
 }
 
--- test-form/src/components/Component.js
+++ test-form/src/components/Component.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import Text from './Text.js'
 import Heading from './Heading.js';
 
 function Component(props) {
     const {
+        componentKey = 'lol',
         title = 'Default Title',
         description = 'Default Description',
         subcomponents = [],
@@ -12,11 +13,71 @@ function Component(props) {
         updateJson
     } = props;
 
+    const [components, setComponents] = useState(null);
+    const [hoveredIndex, setHoveredIndex] = useState(null);
+
+    const addNewComponent = (index) => {
+        // Assuming your new component data is entered through some UI form
+        const newComponent = {
+          Title: "New Component",
+          Content: "New Content",
+          Subcomponents: {}
+        };
+    
+        // Update the state with the new component inserted at the specified index
+        setComponents((prevComponents) => {
+          const newComponents = { ...prevComponents };
+          const newKey = Date.now().toString();
+    
+          // Insert the new component at the specified index
+          const keys = Object.keys(newComponents);
+          keys.splice(index, 0, newKey);
+    
+          // Create a copy of the components with the new component added
+          const updatedComponents = keys.reduce((acc, key) => {
+            acc[key] = key === newKey ? newComponent : newComponents[key];
+            return acc;
+          }, {});
+    
+          return updatedComponents;
+        });
+      };
+    
+    
+      const makeSubcomponent = (index) => {
+        if (index !== null && index > 0) {
+          setComponents((prevComponents) => {
+            const newComponents = { ...prevComponents };
+      
+            // Get the selected component and the one above it
+            const selectedKey = Object.keys(newComponents)[index];
+            const aboveKey = Object.keys(newComponents)[index - 1];
+      
+            // Make a copy of the selected component
+            const selectedComponent = { ...newComponents[selectedKey] };
+      
+            // Remove the selected component from the top level
+            delete newComponents[selectedKey];
+      
+            // Make it a subcomponent of the component above
+            newComponents[aboveKey].Subcomponents = newComponents[aboveKey].Subcomponents || {};
+            newComponents[aboveKey].Subcomponents[selectedKey] = selectedComponent;
+      
+            return newComponents;
+          });
+        }
+      };
+
     const jsonProps = { index, updateJson };
     
         return (
-        <div>
-            <Heading text={props.title} size={level} {...jsonProps} />
+            
+        <div
+            key={componentKey}
+            onMouseEnter={() => setHoveredIndex(index)}
+            onMouseLeave={() => setHoveredIndex(null)}
+          >
+            <Heading text={index + props.title} size={level} {...jsonProps} />
             <Text text={description} {...jsonProps} label = "Content" />
 
             {/* list of subcomponents*/}
@@ -35,6 +96,12 @@ function Component(props) {
                     </li>
                 ))}
             </ul>
+            {hoveredIndex === index && (
+                <div>
+                  <button onClick={() => addNewComponent(index + 1)}> Add New Component </button>
+                  <button onClick={() => makeSubcomponent(index)}> Make Subcomponent </button>
+                </div>
+              ) }
         </div>
     );
 }
